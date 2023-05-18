bd0c4d56afe9b398b40c0c639b6f75eb1415a788
Scott Stargel <stargelnaz@gmail.com>
2023-05-18 13:50:22 +1000

    fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import teamData from './team.json';
 import CalculateResult from './CalculateResult';
+
 const App = () => {
   const [selectedAction, setSelectedAction] = useState(null);
   const [selectedRarity, setSelectedRarity] = useState(null);
@@ -8,15 +9,9 @@ const App = () => {
   const [selectedSubcategories, setSelectedSubcategories] = useState({});
   const [bonusTotal, setBonusTotal] = useState(0);
   const [randomNumber, setRandomNumber] = useState(null);
-
-  const handleRollClick = () => {
-    const random = Math.floor(Math.random() * 100) + 1;
-    setRandomNumber(random);
-  };
-
-  const handleActionClick = (resourceType) => {
-    setSelectedAction(resourceType);
-  };
+  const [orchidRandomNumber, setOrchidRandomNumber] = useState(null);
+  const [bonusRandomNumber, setBonusRandomNumber] = useState(null);
+  const [resourceRandomNumber, setResourceRandomNumber] = useState(null);
 
   const handleRarityClick = (rarity) => {
     const selectedDice = teamData.dice.find(
@@ -24,6 +19,23 @@ const App = () => {
     );
     setSelectedRarity(rarity);
     setSelectedSided(selectedDice.sided);
+  };
+  const handleRollClick = () => {
+    const random = Math.floor(Math.random() * 100) + 1;
+    setRandomNumber(random);
+
+    const orchidRandom = Math.floor(Math.random() * 100) + 1;
+    setOrchidRandomNumber(orchidRandom);
+
+    const bonusRandomNumber = Math.floor(Math.random() * 100) + 1;
+    setBonusRandomNumber(bonusRandomNumber);
+
+    const resourceRandom = Math.floor((Math.random() * selectedSided) / 2) + 1;
+    setResourceRandomNumber(resourceRandom);
+  };
+
+  const handleActionClick = (resourceType) => {
+    setSelectedAction(resourceType);
   };
 
   const handleSubcategoryClick = (category, subcategoryValue) => {
@@ -44,6 +56,14 @@ const App = () => {
 
     setBonusTotal(total);
   };
+  const diePower = selectedSided / 2;
+
+  const orchidWinner = Math.ceil(diePower + (diePower * bonusTotal) / 100);
+
+  const slotReward = (
+    (randomNumber / 100) * diePower +
+    diePower * (bonusTotal / 100)
+  ).toFixed(2);
 
   return (
     <div className='flex'>
@@ -102,11 +122,11 @@ const App = () => {
               </div>
               <p className='mt-4 text-lg'>
                 {selectedRarity
-                  ? `${selectedRarity}'s use ${
+                  ? `${selectedRarity}s use ${
                       teamData.dice.find(
                         (diceItem) => diceItem.rarity === selectedRarity
                       ).sided
-                    }-sided dice`
+                    }-sided die`
                   : 'Choose a Rarity for more information'}
               </p>
             </div>
@@ -200,20 +220,46 @@ const App = () => {
               <p className='mt-4 text-lg'>Total bonus is {bonusTotal}</p>
             </div>
 
-            <div className='py-4'>
-              <h2 className='text-2xl font-bold'>MAIN DICE ROLL</h2>
-              <div className='flex flex-col items-center'>
-                <button
-                  className='py-2 px-4 bg-blue-500 text-white rounded'
-                  onClick={handleRollClick}
-                >
-                  ROLL IT!
-                </button>
-                {randomNumber !== null && (
-                  <div className='mt-4 text-3xl font-bold text-white'>
-                    &nbsp;{randomNumber}&nbsp;
-                  </div>
-                )}
+            <div className='flex flex-row'>
+              {/* MAIN DICE ROLL */}
+              <button
+                className='py-2 px-4 bg-blue-500 text-white rounded'
+                onClick={handleRollClick}
+              >
+                ROLL IT!
+              </button>
+              <div className='py-4 w-6/12'>
+                <h2 className='text-2xl font-bold'>MAIN DICE ROLL</h2>
+                <div className='flex flex-col items-center'>
+                  {randomNumber !== null && (
+                    <div className='mt-4 text-3xl font-bold text-white'>
+                      &nbsp;{randomNumber}&nbsp;
+                    </div>
+                  )}
+                </div>
+              </div>
+
+              {/* BONUS DICE ROLL */}
+              <div className='py-4 w-6/12'>
+                <h2 className='text-2xl '>BONUS DICE ROLL</h2>
+                <div className='flex flex-col items-center'>
+                  {bonusRandomNumber !== null && (
+                    <div className='mt-4 text-3xl font-bold text-gray-500'>
+                      &nbsp;{bonusRandomNumber}&nbsp;
+                    </div>
+                  )}
+                </div>
+              </div>
+              {/* ORCHID DICE ROLL */}
+              <div className='py-4 w-6/12'>
+                <h2 className='text-2xl '>ORCHID DICE ROLL</h2>
+                <div className='flex flex-col items-center'>
+                  {orchidRandomNumber !== null && (
+                    <div className='mt-4 text-3xl font-bold text-white'>
+                      &nbsp;{orchidRandomNumber}&nbsp;
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           </div>
@@ -221,15 +267,8 @@ const App = () => {
       </div>
 
       <div className='w-1/2 bg-gray-200'>
-        <div className='flex flex-col m-5 p-6 border border-blue-500'>
-          <div>Selected Action Resource: {selectedAction}</div>
-          <div>Selected Rarity: {selectedRarity}</div>
-          <div>Selected Sided: {selectedSided}</div>
-          <div>Total Bonus: {bonusTotal}</div>
-          <div>Main Dice Roll (n/100): {randomNumber}</div>
-        </div>
-
-        <div className='flex flex-col m-5 p-6 border border-white bg-blue-300'>
+        {/* --------------------------------------------------------------------------------SLOT RESULT */}{' '}
+        <div className='flex flex-col m-5 p-6 border border-white bg-green-200'>
           <div className='font-bold text-2xl'>Slot Result</div>
           <p>
             <span className='font-bold text-gray-600'>
@@ -237,62 +276,118 @@ const App = () => {
             </span>
           </p>
           <p>
-            <span className='font-bold'>HOW IS IT CALCULATED?</span> A bell
-            curve based on the main dice roll will choose a value between 0.5
-            and the selectedSided value divided in half.
+            <span className='font-bold'>HOW IS IT CALCULATED?</span>Guaranteed
+            amount resources. Rarity die is multiplied by
           </p>
+          <div className='bg-yellow-800'>WHAT DID YOU WIN?</div>
           <div>
-            <CalculateResult
-              randomNumber={randomNumber}
-              selectedSided={selectedSided}
-            />
+            <div>
+              {/* <table className='mx-auto border border-collapse'> */}
+              <table>
+                <tr>
+                  <th className='border border-gray-400 p-2 text-center'>
+                    Main Roll
+                  </th>
+                  <th className='border border-gray-400 p-2 text-center'>
+                    {selectedSided}-sided Roll
+                  </th>
+                  <th className='border border-gray-400 p-2 text-center'>
+                    Team Bonus
+                  </th>
+                </tr>
+                <tr>
+                  <td className='border border-gray-400 p-2 text-center'>
+                    {randomNumber}%
+                  </td>
+                  <td className='border border-gray-400 p-2 text-center'>
+                    {resourceRandomNumber}
+                  </td>
+                  <td className='border border-gray-400 p-2 text-center'>
+                    {bonusTotal}%
+                  </td>
+                </tr>
+              </table>
+            </div>
           </div>
+          <div>Slot Reward: {slotReward}</div>
         </div>
-        <div>
-          SLOT RESULTS:
-          <p className='italic'>
-            These results are based on the ACTION that was chosen.
-            <br /> It will produce the associated RESOURCE in various amounts
-            ranging from 0.25 to the RARITY DIE then divided in half
-            <br />
-            The concept is that the Guide, Beast will be looking for their
-            primary resource. Only that resource will show in the SLOT RESULTS
-          </p>
-        </div>
-        <div>BONUS RESULTS:</div>
-        <p className='italic'>
-          These results are based on a much smaller chance of finding something
-          OTHER than the resource associated with the ACTION. The idea here is
-          that occassionally while looking for FOOD a GUIDE may find PETAL.
-          However, the BONUS RESULTS can be improved based on the TEAM BONUSES
-        </p>
-        {/* --------------------------------------------------------------------------------SWAPLING RESULT */}
-        <div className='flex flex-col m-5 p-6 border border-white bg-blue-300'>
-          <div className='font-bold text-2xl'>Swapling Orchid Result</div>
+        {/* --------------------------------------------------------------------------------BONUS RESULT */}
+        <div className='flex flex-col m-5 p-6 border border-white bg-green-300'>
+          <div className='font-bold text-2xl'>Bonus Resource Result</div>
           <p>
             <span className='font-bold text-gray-600'>
-              Factors: Orchid Dice Roll, Selected Sided, Team Bonus
+              Factors: Main Dice Roll, Bonus Dice Roll
             </span>
           </p>
           <p>
-            <span className='font-bold'>HOW IS IT CALCULATED?</span> Orchid Roll
-            &le; selectedSided * bonusTotal
+            <span className='font-bold'>HOW IS IT CALCULATED?</span> If the MAIN
+            DICE ROLL is greater than 75% then a BONUS ROLL will determine the
+            resources won
           </p>
+
           <div>
             {randomNumber === null ||
             randomNumber === 0 ||
             randomNumber === undefined ? (
-              <div>TICKET RESULT: Waiting on Main Dice Roll...</div>
-            ) : randomNumber > 95 ? (
-              <div>TICKET RESULT: WINNER!</div>
+              <div>BONUS RESULT: Waiting on Dice Roll...</div>
+            ) : randomNumber > 75 ? (
+              <div>
+                <div className='bg-yellow-500'>BONUS RESULT: WINNER!</div>
+                <div className='bg-yellow-800'>WHAT DID YOU WIN?</div>
+              </div>
             ) : (
-              <div>TICKET RESULT: Nope</div>
+              <div className='bg-red-500'>BONUS RESULT: Nope</div>
             )}
           </div>
         </div>
+        {/* --------------------------------------------------------------------------------SWAPLING RESULT */}
+        <div className='flex flex-col m-5 p-6 border border-white bg-green-400'>
+          <div className='font-bold text-2xl'>Swapling Orchid Result</div>
+          <p>
+            <span className='font-bold text-gray-600'>
+              Factors: Orchid Die Roll, Die Power, Team Bonus
+            </span>
+          </p>
+          <p>
+            <span className='font-bold'>HOW IS SUCCESS CALCULATED?</span>A new
+            roll orchidRoll is generated. Success if the Orchid Die Roll &le;
+            diePower (1/2 of Rarity Die) + teamPower (diePower * bonusPower)
+          </p>
+          <p>
+            {selectedRarity === null || selectedRarity === undefined ? (
+              <div>Waiting on rarity selection...</div>
+            ) : (
+              <div>Die power is {diePower}</div>
+            )}
 
+            <div
+              className={`${
+                bonusTotal < 0
+                  ? 'bg-red-500 text-white'
+                  : 'bg-transparent text-black'
+              }`}
+            >
+              Team power is {bonusTotal}%
+            </div>
+
+            <div>orchidWinner is {orchidWinner}</div>
+            <div>orchidRandomNumber is {orchidRandomNumber}</div>
+          </p>
+
+          <div>
+            {orchidWinner === null ||
+            orchidWinner === 0 ||
+            orchidWinner === undefined ? (
+              <div>TICKET RESULT: Waiting on Dice Roll</div>
+            ) : orchidRandomNumber <= orchidWinner ? (
+              <div className='bg-yellow-500'>TICKET RESULT: WINNER!</div>
+            ) : (
+              <div className='bg-red-500'>TICKET RESULT: Nope</div>
+            )}
+          </div>
+        </div>
         {/* --------------------------------------------------------------------------------TICKET RESULT */}
-        <div className='flex flex-col m-5 p-6 border border-white bg-blue-300'>
+        <div className='flex flex-col m-5 p-6 border border-white bg-green-500'>
           <div className='font-bold text-2xl'>Hunt Ticket Result</div>
           <p>
             <span className='font-bold text-gray-600'>
@@ -300,19 +395,34 @@ const App = () => {
             </span>
           </p>
           <p>
-            <span className='font-bold'>HOW IS IT CALCULATED?</span> If the roll
-            is > 95
+            <span className='font-bold'>HOW IS IT CALCULATED?</span> If the MAIN
+            DICE ROLL is > 95
           </p>
+
           <div>
             {randomNumber === null ||
             randomNumber === 0 ||
             randomNumber === undefined ? (
               <div>TICKET RESULT: Waiting on Main Dice Roll...</div>
             ) : randomNumber > 95 ? (
-              <div>TICKET RESULT: WINNER!</div>
+              <div className='bg-yellow-500'>TICKET RESULT: WINNER!</div>
             ) : (
-              <div>TICKET RESULT: Nope</div>
+              <div className='bg-red-500'>TICKET RESULT: Nope</div>
             )}
+          </div>
+        </div>
+        {/* --------------------------------------------------------------------------------VARIABLES */}{' '}
+        <div className='flex flex-col m-5 p-6 border border-blue-500'>
+          <div>selectedAction: {selectedAction}</div>
+          <div>
+            selectedRarity: {selectedRarity} → selectedSided: {selectedSided} →
+            diePower: {diePower}
+          </div>
+
+          <div>bonusTotal: {bonusTotal}%</div>
+          <div>
+            randomNumber: {randomNumber} → bonusRandomNumber:
+            {bonusRandomNumber} → orchidRandomNumber: {orchidRandomNumber}
           </div>
         </div>
       </div>
